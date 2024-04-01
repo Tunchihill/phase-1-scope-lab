32269b420d7e417002a27789042cf7361db1dec0
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-04-01 22:19:22 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+//Write your solution in this file
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+   console.log(bestCustomer = "not bob");
+}
+
+setBestCustomer(); 
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+//overwriteBestCustomer(); 
+//console.log(bestCustomer);
+
+const leastFavoriteCustomer = "James"; 
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not James";
+}
+
+console.log(changeLeastFavoriteCustomer())
